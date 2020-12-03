9b67b6aebe443f679e042d2bbdb4ec28b54c20b3
Nicolas Gomez <gomez.nicolas.martin@gmail.com>
2020-12-03 01:09:27 -0300

    Procfile

--- src/main/java/model/RepositorioNotificaciones.java
+++ src/main/java/model/RepositorioNotificaciones.java
@@ -7,53 +7,43 @@ import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
 
 import dominio.Notificacion;
 import dominio.Usuario;
-import dominio.dao.DAO;
-import dominio.dao.DAOHibernate;
 
 public class RepositorioNotificaciones implements WithGlobalEntityManager {
 
 	List<Notificacion> notificaciones = new ArrayList<Notificacion>();
-	DAO<Notificacion> dao = new DAOHibernate<>(Notificacion.class);
-	
-    private static RepositorioNotificaciones instance = new RepositorioNotificaciones();
 
-	
-    public static RepositorioNotificaciones getInstance(){
-        if (instance == null) 
-        	instance = new RepositorioNotificaciones(); 
-  
-        return instance;
-     }
-    
-    private RepositorioNotificaciones(){}
-    
-    public List<Notificacion> obtenerNotificaciones(Usuario usuario)
-    {
-            List<Notificacion> notificaciones = entityManager()
-                .createQuery("from Notificacion", Notificacion.class)
-                .getResultList();
-           
-            return notificaciones;
-    }
-    
-    public Notificacion buscarNotificacion(Integer id)
-    {
-            return entityManager().find(Notificacion.class, id);
-   }
-    
-    
-    public void borrarNotificacion(Notificacion notificacion)
-    {
-    	entityManager().getTransaction().begin();
-        entityManager().remove(notificacion);
-        entityManager().getTransaction().commit();
-    	      
-    } 
-    
-    public void limpiarRepositorio()
-    {
-    	this.notificaciones.clear();
-    }
-    
-	
+	private static RepositorioNotificaciones instance = new RepositorioNotificaciones();
+
+	public static RepositorioNotificaciones getInstance() {
+		if (instance == null)
+			instance = new RepositorioNotificaciones();
+
+		return instance;
+	}
+
+	private RepositorioNotificaciones() {
+	}
+
+	public List<Notificacion> obtenerNotificaciones(Usuario usuario) {
+		List<Notificacion> notificaciones = entityManager().createQuery("from Notificacion", Notificacion.class)
+				.getResultList();
+
+		return notificaciones;
+	}
+
+	public Notificacion buscarNotificacion(Integer id) {
+		return entityManager().find(Notificacion.class, id);
+	}
+
+	public void borrarNotificacion(Notificacion notificacion) {
+		entityManager().getTransaction().begin();
+		entityManager().remove(notificacion);
+		entityManager().getTransaction().commit();
+
+	}
+
+	public void limpiarRepositorio() {
+		this.notificaciones.clear();
+	}
+
 }
